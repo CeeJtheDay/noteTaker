7b701800365ea46f0b7eb259829db66bf1bcbddc
CJ Conrad <carlj.conrad@CJs-MacBook-Pro.local>
2019-12-07 09:55:54 -0800

    index.js saved

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 let notes = require("./db/db.json");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 80;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
